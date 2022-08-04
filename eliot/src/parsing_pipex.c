07cccc27bf5b362befbf3fec3bcd3ab77d3a68b6
Eliot EDY <eedy@e2r12p12.clusters.42paris.fr>
2022-08-04 19:43:40 +0200

    up

--- eliot/src/parsing_pipex.c
+++ eliot/src/parsing_pipex.c
@@ -6,7 +6,7 @@
 /*   By: eedy <eliot.edy@icloud.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 16:43:53 by eedy              #+#    #+#             */
-/*   Updated: 2022/08/04 19:41:43 by eedy             ###   ########.fr       */
+/*   Updated: 2022/08/04 19:43:09 by eedy             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ truc a faire pour demain:
 	- coder les fonctiosn pour les listes chainees (init, add, clear)
 	- recuperer la fonction pour ajouter des char et la struc de theo
 	- creer une foncion qui lit tout une fois et ajoute au fur et a mesure dans la lister chainee
+	- <<her >test << non >> pk cat test < Makefile >"dan"s
 
 int	parsing_fork(t_pipex *pipex) // je recommence pour parser tout d'abbord pour faciliter les choses
 {
